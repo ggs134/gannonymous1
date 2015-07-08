03a3ba4d05ebcc58235f66b9d043d0345b8b87b6
ggs134 <inditow@gmail.com>
2015-07-08 23:04:13 +0900

    orm기반으로 수정

--- flaskr.py
+++ flaskr.py
@@ -1,10 +1,14 @@
 #-*-coding:utf-8-*-
 # all the import
-from __future__ import with_statement
+# from __future__ import with_statement
 #import sqlite3
-import MySQLdb as mariadb
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from contextlib import closing
+from sqlalchemy import create_engine, desc, asc
+from sqlalchemy.orm import sessionmaker, scoped_session
+from db_setup import Base, Entries, Board1, Board2
+# import MySQLdb as mariadb
+from flask import Flask, request, g, redirect, url_for, abort, render_template, flash
+from flask import session as login_session
+# from contextlib import closing
 import sys
 
 #configuration
@@ -20,8 +24,17 @@ app.config.from_object(__name__)
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def connect_db():
-	return mariadb.connect('localhost','root', 'wjdtnsgud1!','annonymous')
+#Connect to Database and create database session
+engine = create_engine("mysql://root:wjdtnsgud1!@localhost/annonymous", encoding='utf8', echo=False)
+Base.metadata.bind = engine
+
+# DBSession = sessionmaker()
+DBSession=scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+# DBSession.configure(bind=engine)
+session = DBSession()
+
+# def connect_db():
+# 	return mariadb.connect('localhost','root', 'wjdtnsgud1!','annonymous')
 #	return sqlite3.connect(app.config['DATABASE'])
 
 # def init_db():
@@ -40,60 +53,54 @@ def connect_db():
 # #	g.db.close()
 # 	con.close()
 
+# @app.route('/')
+# def show_entries():
+# 	con=connect_db()
+# 	cursor=con.cursor()
+# 	cursor.execute('select title, text from entries order by id desc')
+# 	entries=[dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
+# 	cursor.close()
+# 	con.close()
+# 	return render_template('show_entries.html', entries=entries)
+
 @app.route('/')
 def show_entries():
-#	cur=g.db.execute('select title, text from entries order by id desc')
-	con=connect_db()
-	cursor=con.cursor()
-	cursor.execute('select title, text from entries order by id desc')
-	entries=[dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
-	cursor.close()
-	con.close()
-	return render_template('show_entries.html', entries=entries)
+	entries=session.query(Entries).order_by(desc(Entries.id)).all()
+	session.close()
+	return render_template('show_entries.html',entries=entries)
+
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-	# if not session.get('logged_in'):
-	# 	abort(401)
-	con=connect_db()
-	cursor=con.cursor()
-	cursor.execute('insert into entries (title, text) values (%s,%s)',[request.form['title'].encode('utf-8'), request.form['text'].encode('utf-8')])
-	con.commit()
-	cursor.close()
-	con.close()
-#	con.cursor().execute('insert into entries (title, text) values (?,?)',[request.form['title'], request.form['text']])
-#	con.commit()
+	# con=connect_db()
+	# cursor=con.cursor()
+	# cursor.execute('insert into entries (title, text) values (%s,%s)',[request.form['title'].encode('utf-8'), request.form['text'].encode('utf-8')])
+	# con.commit()
+	# cursor.close()
+	# con.close()
+	newEntry=Entries(title=request.form['title'].encode('utf-8'), text=request.form['text'].encode('utf-8'))
+	session.add(newEntry)
+	session.commit()
 	flash('글이 게시되었어!')
+	session.close()
 	return redirect(url_for('show_entries'))
 
 @app.route('/add1', methods=['POST'])
 def add1():
-	# if not session.get('logged_in'):
-	# 	abort(401)
-	con=connect_db()
-	cursor=con.cursor()
-	cursor.execute('insert into board1 (title, text) values (%s,%s)',[request.form['title'].encode('utf-8'), request.form['text'].encode('utf-8')])
-	con.commit()
-	cursor.close()
-	con.close()
-#	con.cursor().execute('insert into entries (title, text) values (?,?)',[request.form['title'], request.form['text']])
-#	con.commit()
-	flash('글이 올라갔어!')
+	newBoard1=Board1(title=request.form['title'].encode('utf-8'), text=request.form['text'].encode('utf-8'))
+	session.add(newBoard1)
+	session.commit()
+	flash('글이 게시되었어!')
+	session.close()
 	return redirect(url_for('board1'))
 
 @app.route('/add2', methods=['POST'])
 def add2():
-	# if not session.get('logged_in'):
-	# 	abort(401)
-	con=connect_db()
-	cursor=con.cursor()
-	cursor.execute('insert into board2 (title, text) values (%s,%s)',[request.form['title'].encode('utf-8'), request.form['text'].encode('utf-8')])
-	con.commit()
-	cursor.close()
-	con.close()
-#	con.cursor().execute('insert into entries (title, text) values (?,?)',[request.form['title'], request.form['text']])
-#	con.commit()
-	flash('글이 올라갔어!')
+	newBoard2=Board2(title=request.form['title'].encode('utf-8'), text=request.form['text'].encode('utf-8'))
+	session.add(newBoard2)
+	session.commit()
+	flash('글이 게시되었어!')
+	session.close()
 	return redirect(url_for('board2'))
 
 @app.route('/login', methods=['GET','POST'])
@@ -105,36 +112,32 @@ def login():
 		elif request.form['password'] != app.config['PASSWORD']:
 			error='invalid password'
 		else:
-			session['logged_in']=True
+			login_session['logged_in']=True
 			flash('로그인 되었습니다.')
 			return redirect(url_for('show_entries'))
 	return render_template('login.html', error=error)
 	
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
+	login_session.pop('logged_in', None)
 	flash('로그아웃되었습니다.')
 	return redirect(url_for('show_entries'))
 
 @app.route('/board1')
 def board1():
-	con=connect_db()
-	cursor=con.cursor()
-	cursor.execute('select title,text from board1 order by id desc')
-	writings1=[dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
-	cursor.close()
-	con.close()
+	writings1=session.query(Board1).order_by(desc(Board1.id)).all()
+	session.close()
 	return render_template('board1.html', writings1=writings1)
 
 @app.route('/board2')
 def board2():
-	con=connect_db()
-	cursor=con.cursor()
-	cursor.execute('select title,text from board2 order by id desc')
-	writings2=[dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
-	cursor.close()
-	con.close()
+	writings2=session.query(Board2).order_by(desc(Board2.id)).all()
+	session.close()
 	return render_template('board2.html', writings2=writings2)
+
+@app.teardown_request
+def shutdown_session(exception=None):
+	DBSession.remove()
 
 if __name__=='__main__':
 	app.run(host='0.0.0.0')
