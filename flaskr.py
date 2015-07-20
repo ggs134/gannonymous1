958518fe2167d334f6cf0f288de22d1a3d86655d
ggs134 <inditow@gmail.com>
2015-07-20 10:13:38 +0900

    맨 위로 버튼, 글목록 페이지 업데이트

--- flaskr.py
+++ flaskr.py
@@ -1,21 +1,17 @@
 #-*-coding:utf-8-*-
+
 # all the import
-# from __future__ import with_statement
-#import sqlite3
-from sqlalchemy import create_engine, desc, asc
+from sqlalchemy import create_engine, desc, asc, func
 from sqlalchemy.orm import sessionmaker, scoped_session
 from db_setup import Base, Entries, Board1, Board2
-# import MySQLdb as mariadb
-from flask import Flask, request, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, redirect, url_for, abort, render_template, flash
 from flask import session as login_session
-# from contextlib import closing
 import sys
 
 #configuration
-# DATABASE = '/tmp/flaskr.db'
 DEBUG = True
 SECRET_KEY = '1234'
-USERNAME = 'admin'
+USERNAME = 'adminJ'
 PASSWORD = 'wjdtnsgud1!'
 
 #create our little application
@@ -27,49 +23,53 @@ sys.setdefaultencoding('utf-8')
 #Connect to Database and create database session
 engine = create_engine("mysql://root:wjdtnsgud1!@localhost/annonymous", encoding='utf8', echo=False)
 Base.metadata.bind = engine
-
-# DBSession = sessionmaker()
 DBSession=scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-# DBSession.configure(bind=engine)
 session = DBSession()
 
-# def connect_db():
-# 	return mariadb.connect('localhost','root', 'wjdtnsgud1!','annonymous')
-#	return sqlite3.connect(app.config['DATABASE'])
-
-# def init_db():
-# 	with closing(connect_db()) as db:
-# 		with app.open_resource('schema.sql') as f:
-# 			db.cursor().executescript(f.read())
-# 		db.commit()
-
-# @app.before_request
-# def before_request():
-# 	con=connect_db()
-# #	g.db=connect_db()
-	
-# @app.teardown_request
-# def teardown_request(exception):
-# #	g.db.close()
-# 	con.close()
-
-# @app.route('/')
-# def show_entries():
-# 	con=connect_db()
-# 	cursor=con.cursor()
-# 	cursor.execute('select title, text from entries order by id desc')
-# 	entries=[dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
-# 	cursor.close()
-# 	con.close()
-# 	return render_template('show_entries.html', entries=entries)
-
+#main page
 @app.route('/')
 def show_entries():
 	entries=session.query(Entries).order_by(desc(Entries.id)).all()
 	session.close()
 	return render_template('show_entries.html',entries=entries)
+ 
+#board1 page
+@app.route('/board1/<int:page_number>')
+def board1(page_number):
+	#총 글의 갯수를 구함
+	total_number_writings=int(session.query(func.count('*')).select_from(Board1).scalar())
+	# 왜 +2를 해야 하지?? 총 페이지 수를 도출
+	total_page_number=int(total_number_writings/10)+2
+	if page_number>=total_page_number:
+		flash('그 페이지는 존재하지 않습니다')
+		return redirect(url_for('board1', page_number=1))
+	# count number of writings
+	# session.query(Entries.id).order_by(desc(Entries.id)).first()
+	if page_number==None:
+		page_number=1
+	writings1=session.query(Board1).order_by(desc(Board1.id)).limit(10).offset((page_number-1)*10).all()
+	session.close()
+	return render_template('board1.html', writings1=writings1, page_number=page_number, total_page_number=total_page_number)
 
+#board2 page
+@app.route('/board2/<int:page_number>')
+def board2(page_number):
+	#총 글의 갯수를 구함
+	total_number_writings=int(session.query(func.count('*')).select_from(Board2).scalar())
+	# 왜 +2를 해야 하지?? 총 페이지 수를 도출
+	total_page_number=int(total_number_writings/10)+2
+	if page_number>=total_page_number:
+		flash('그 페이지는 존재하지 않습니다')
+		return redirect(url_for('board2', page_number=1))
+	# count number of writings
+	# session.query(Entries.id).order_by(desc(Entries.id)).first()
+	if page_number==None:
+		page_number=1
+	writings2=session.query(Board2).order_by(desc(Board2.id)).limit(10).offset((page_number-1)*10).all()
+	session.close()
+	return render_template('board2.html', writings2=writings2, page_number=page_number, total_page_number=total_page_number)
 
+#add new notice
 @app.route('/add', methods=['POST'])
 def add_entry():
 	# con=connect_db()
@@ -85,6 +85,7 @@ def add_entry():
 	session.close()
 	return redirect(url_for('show_entries'))
 
+#add new post in board1
 @app.route('/add1', methods=['POST'])
 def add1():
 	newBoard1=Board1(title=request.form['title'].encode('utf-8'), text=request.form['text'].encode('utf-8'))
@@ -92,8 +93,9 @@ def add1():
 	session.commit()
 	flash('글이 게시되었어!')
 	session.close()
-	return redirect(url_for('board1'))
+	return redirect(url_for('board1',page_number=1))
 
+#add new post in board2
 @app.route('/add2', methods=['POST'])
 def add2():
 	newBoard2=Board2(title=request.form['title'].encode('utf-8'), text=request.form['text'].encode('utf-8'))
@@ -101,8 +103,9 @@ def add2():
 	session.commit()
 	flash('글이 게시되었어!')
 	session.close()
-	return redirect(url_for('board2'))
+	return redirect(url_for('board2',page_number=1))
 
+#admin login page
 @app.route('/login', methods=['GET','POST'])
 def login():
 	error=None
@@ -116,24 +119,13 @@ def login():
 			flash('로그인 되었습니다.')
 			return redirect(url_for('show_entries'))
 	return render_template('login.html', error=error)
-	
+
+#admin logout
 @app.route('/logout')
 def logout():
 	login_session.pop('logged_in', None)
 	flash('로그아웃되었습니다.')
 	return redirect(url_for('show_entries'))
-
-@app.route('/board1')
-def board1():
-	writings1=session.query(Board1).order_by(desc(Board1.id)).all()
-	session.close()
-	return render_template('board1.html', writings1=writings1)
-
-@app.route('/board2')
-def board2():
-	writings2=session.query(Board2).order_by(desc(Board2.id)).all()
-	session.close()
-	return render_template('board2.html', writings2=writings2)
 
 @app.teardown_request
 def shutdown_session(exception=None):
